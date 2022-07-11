7352092bb4d0c0efe1e5b2df9373202afdffb21c
jfpablo <103375465+jfpablo@users.noreply.github.com>
2022-07-11 18:31:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ setTimeout(function(){
 },2000);
 function loaded()
 {
-  cont.innerHTML = "une erreur s'est produite"
+  cont.innerHTML = "une erreur s'est produite";
+    accueil.innerHTML = "accueil";
+    accueil = "selected";
+    if (accueil === "selected")
+    {
+        accueil.style.background = "blue";
+    }
+    else
+    {
+        accueil.style.background = "white";
+    }
 }
