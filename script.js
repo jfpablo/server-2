205401e7d22d11c6b05903cd244da737090354d6
jfpablo <103375465+jfpablo@users.noreply.github.com>
2022-07-11 18:38:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,5 @@ function loaded()
 {
   cont.innerHTML = "une erreur s'est produite";
     accueil.innerHTML = "accueil";
-    let accueil1 = "selected";
-    if (accueil1 === "selected")
-    {
         accueil.style.background = "blue";
-    }
-    else
-    {
-        accueil.style.background = "white";
-    }
 }
